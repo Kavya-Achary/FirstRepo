c5f1b0a8080b12915f3be79d2a3a720a6ca22e87
Kavya-Achary <Kavyaachary2511@gmail.com>
2024-03-18 23:41:06 +0530

    commit 23

--- CostTool-getCostData/getCosts.js
+++ CostTool-getCostData/getCosts.js
@@ -67,7 +67,7 @@ const getCostData = async (
     // test 5
     // test 6
     // test 11
-    //test 17
+    //test 21
 
     let filterIDArrayDRY, filterIDArrayREFER = [];
     if(foundDry.length){
