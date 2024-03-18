f14f9bd112cf397cde8f5634f0f2f491b3ad0ef2
Kavya-Achary <Kavyaachary2511@gmail.com>
2024-03-18 15:54:24 +0530

    newcommit11

--- CostTool-getCostData/getCosts.js
+++ CostTool-getCostData/getCosts.js
@@ -66,7 +66,7 @@ const getCostData = async (
     // test 4
     // test 5
     // test 6
-    // test 8
+    // test 10
 
     let filterIDArrayDRY, filterIDArrayREFER = [];
     if(foundDry.length){
--- CostTool-getFilterData/getFilters.js
+++ CostTool-getFilterData/getFilters.js
@@ -12,7 +12,7 @@ const runQuery = async(pool, query) => {
 
 const getFilters = async(pool, error_handler, callback) =>{
     /// BUILD RESPONSE JSON OBJ - TODO - USE ORM?
-    // test2
+    // test5
 
     let returnJSON = {};
     let query;
